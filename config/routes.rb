7e741c348dd40c396f9cfa53a1bbd7cb3a74cd09
RDM Cal <mrbandido@gmail.com>
2019-12-05 02:33:07 -0800

    updates for heroku deployment

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  # root is setting the root route for the application
+  root 'cars#index'
   resources :cars
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
